ccae88c39553452d0ab6da1d248de290ce824bfe
Ivan Rodriguez <40413833+IvanRod1@users.noreply.github.com>
2023-05-02 14:05:27 -0300

    Add files via upload

---
+++ Practico 6/Practico 6.hs
@@ -0,0 +1,7 @@
+import PriorityQueue
+
+{-instance Eq Persona Where
+  p1 == p2 = edad p1 == edad p2
+                                  ejemplo persona fidel
+  instance Ord Persona where
+  p1 <= p2 = edad p1 <= edad p2-}
---
+++ Practico 6/PriorityQueue.hs
@@ -0,0 +1,29 @@
+module PriorityQueue
+(PriorityQueue,emptyPQ,isEmptyPQ,insertPQ,findMinPQ,deleteMinPQ)
+where
+data PriorityQueue a = PQ [a]
+
+emptyPQ :: PriorityQueue a
+isEmptyPQ :: PriorityQueue a -> Bool
+insertPQ :: Ord a => a -> PriorityQueue a -> PriorityQueue a
+findMinPQ :: Ord a => PriorityQueue a -> a
+deleteMinPQ :: Ord a => PriorityQueue a -> PriorityQueue a
+
+emptyPQ = PQ []
+isEmptyPQ (PQ xs) = null xs
+insertPQ e (PQ xs) = PQ (e:xs)
+findMinPQ (PQ xs) = minimum xs
+deleteMinPQ (PQ xs) = PQ (borrarMin xs)
+
+borrarMin :: Ord a => [a] -> [a]
+--Precondicion: La lista no esta vacia
+borrarMin xs = borrar (minimum xs) xs
+
+borrar :: Eq a => a -> [a] -> [a]
+borrar x [] = []
+borrar x (y:ys) = if x == y then ys else y : borrar x ys
+
+heapSort :: Ord a => [a] -> [a]
+heapSort [] = 
+heapSort (x:xs) = if x < minimum xs
+                  then........
