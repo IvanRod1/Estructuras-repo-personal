dcc122a6c2bdbfcd97e9388f48a619dc1dbfe7ae
Ivan Rodriguez <40413833+IvanRod1@users.noreply.github.com>
2023-05-09 17:45:27 -0300

    Add files via upload

--- Practico 7/Practico 7.hs
+++ Practico 7/Practico 7.hs
@@ -33,7 +33,7 @@ insertBST x (NodeT y ti td) = if x == y
 deleteBST :: Ord a => a -> Tree a -> Tree a
 {-Propósito: dado un BST borra un elemento en el árbol.
 Costo: O(log N)-}
-deleteBST x EmptyT =
+deleteBST x EmptyT = EmptyT
 deleteBST x (NodeT y ti td) = if x == y 
                               then rearmarBST ti td
                               else if x < y
@@ -67,13 +67,13 @@ Costo: O(N2) -}
 esBST EmptyT = False
 esBST (NodeT x ti td) = todosSonMenoresA x ti && todosSonMayoresA x td
 
-todosSonMenoresA :: Ord a => a -> Tree a -> Bool --O(n)
+todosSonMenoresA :: Ord a => a -> Tree a -> Bool --O(n)??
 {-Precondicion:..-}
 todosSonMenoresA _ EmptyT = False
 todosSonMenoresA x (NodeT y ti td) = x < y && todosSonMenoresA x ti && todosSonMenoresA x td
 
 
-todosSonMayoresA :: Ord a => a -> Tree a -> Bool  --O(n)
+todosSonMayoresA :: Ord a => a -> Tree a -> Bool  --O(n)??
 {-Precondicion:..-}
 todosSonMayoresA _ EmptyT = False
 todosSonMayoresA x (NodeT y ti td) = x > y && todosSonMayoresA x ti && todosSonMayoresA x td
@@ -105,7 +105,62 @@ nodo la diferencia de alturas entre el subarbol izquierdo y el derecho es menor 
 Costo: O(N2)-}
 
 balanceado EmptyT = False
-balanceado (NodeT _ ti td) = (heightT ti - heightT td) <= 1
+balanceado (NodeT _ ti td) = (heightT ti - heightT td) <= 1 
+
+---------------------------------------------------------------------
+data Map k v = EmptyT | NodeT (k,v) (Map k v) (Map k v) 
+
+
+emptyM :: Map k v
+{-Costo: O(1)-}
+emptyM = M [] EmptyT EmptyT
+
+assocM :: Ord k => k -> v -> Map k v -> Map k v
+{-Costo: O(log K)-}
+assocM x y EmptyT = NodeT (x,y) EmptyT EmptyT 
+assocM x y (NodeT (k,v) ti td) = if x == k 
+                                 then NodeT (x,y) ti td
+                                 else if x < k 
+                                    then assocM x y ti
+                                 else assocM x y td --NodeT (k,v) (assocM x y ti) (assocM x y td)  
+
+lookupM :: Ord k => k -> Map k v -> Maybe v
+{-Costo: O(log K).-}
+lookupM x EmptyT = Nothing
+lookupM x (NodeT (k,v) ti td) = if x == k
+                                then Just v
+                                else if x < K
+                                  then lookupM x ti
+                                else lookupM x td
+
+
+
+deleteM :: Ord k => k -> Map k v -> Map k v
+{-Costo: O(log K).-}
+deleteM EmptyT =
+deleteM x (NodeT (k,v) ti td) = if x == k 
+                                then
+
+
+{-assocM k v (M kvs) = M (agregarClaveValorA k v kvs)
+
+agregarClaveValorA :: Eq k => k -> v -> [(k,v)] -> [(k,v)]
+agregarClaveValorA k v [] = [(k,v)] 
+agregarClaveValorA k v ((x,y):xys) = if k == x
+                                     then (k,v) : xys
+                                     else (x,y) : agregarClaveValorA k v xys
+
+
+lookupM :: Ord k => k -> Map k v -> Maybe v
+{-Costo: O(log K).-}
+
+lookupM k (M kvs) = valorDe k kvs
+
+valorDe :: Eq k => k -> Tree a -> Maybe v
+valorDe k [] = Nothing
+valorDe k [(x,y):xys] = if k == x
+                        then Just y
+                        else valorDe k xys-}
 
 
 
