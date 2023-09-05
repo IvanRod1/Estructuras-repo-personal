56a25a315bc4ef88a4d2799811dba4d94ad4908d
Ivan Rodriguez <40413833+IvanRod1@users.noreply.github.com>
2023-09-05 15:29:11 -0300

    Add files via upload

--- Estructuras 2/tp-3.hs
+++ Estructuras 2/tp-3.hs
@@ -31,10 +31,11 @@ ponerN x c celda = Bolita c (ponerN (x - 1) c celda)
 
 -----------------------------------------------------------------------------------------
 
-data Objeto = Cacharro | Tesoro
-data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
+data Objeto = Cacharro | Tesoro deriving Show 
+data Camino = Fin | Cofre [Objeto] Camino | Nada Camino deriving Show
 
 camino = Nada (Cofre [Cacharro,Cacharro] (Nada (Cofre [Tesoro] Fin)))
+camino2 = Fin 
 
 -- (Nada (Cofre [Tesoro] Fin)
 hayTesoro :: Camino -> Bool
@@ -72,5 +73,88 @@ cantidadDeTesorosEnCamino (Nada c) = cantidadDeTesorosEnCamino c
 cantidadDeTesorosEnCamino (Cofre obj c) = if hayObjetoValioso obj then 1 + cantidadDeTesorosEnCamino c else cantidadDeTesorosEnCamino c
 
 ----------------------------------------------------------------------------------------------------
+camino3 = Nada (Cofre [Tesoro](Nada (Cofre [Tesoro] (Nada (Cofre [Tesoro] Fin)))))
 cantTesorosEntre :: Int -> Int -> Camino -> Int
+cantTesorosEntre x y c = cantidadDeTesorosHaciendo_Pasos (y - x) (avanzar_Pasos x c)
 
+-- (avanzar_Pasos x c)
+avanzar_Pasos :: Int -> Camino -> Camino
+avanzar_Pasos x c = if x > 0 && not (esFin c) then avanzar_Pasos (x - 1) (detectorCamino c) else c  
+
+detectorCamino :: Camino -> Camino
+detectorCamino Fin = Fin
+detectorCamino (Cofre _ c) = c
+detectorCamino (Nada c) = c
+
+esFin :: Camino -> Bool
+esFin Fin = True
+esFin _ = False 
+
+cantidadDeTesorosHaciendo_Pasos :: Int -> Camino -> Int 
+cantidadDeTesorosHaciendo_Pasos x Fin = 0 
+cantidadDeTesorosHaciendo_Pasos x (Cofre obj c) = if hayObjetoValioso obj && x >= 0 then 1 + cantidadDeTesorosHaciendo_Pasos (x - 1) c else cantidadDeTesorosHaciendo_Pasos (x - 1) c
+cantidadDeTesorosHaciendo_Pasos x (Nada c) = cantidadDeTesorosHaciendo_Pasos (x - 1) c
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a) deriving Show
+
+arbol1 :: Tree Int 
+arbol1 = NodeT 1 (NodeT 2 EmptyT EmptyT) (NodeT 5 (NodeT 20 EmptyT EmptyT) EmptyT)
+
+sumarT :: Tree Int -> Int
+sumarT EmptyT = 0
+sumarT (NodeT x t1 t2) = x + sumarT t1 + sumarT t2
+
+--------------------------------------------------------
+
+sizeT :: Tree a -> Int
+sizeT EmptyT = 0
+sizeT (NodeT _ t1 t2) =  1 + sizeT t1 + sizeT t2
+
+--------------------------------------------------------
+mapDobleT :: Tree Int -> Tree Int
+mapDobleT EmptyT = EmptyT
+mapDobleT (NodeT x t1 t2) = NodeT (x * 2) (mapDobleT t1) (mapDobleT t2)
+
+--------------------------------------------------------
+
+perteneceT :: Eq a => a -> Tree a -> Bool
+perteneceT x EmptyT = False
+perteneceT x (NodeT y t1 t2) = x == y || perteneceT x t1 || perteneceT x t2
+
+----------------------------------------------------------------------------------
+
+leaves :: Tree a -> [a]
+leaves EmptyT = []
+leaves (NodeT x t1 t2) = if esArbolVacio t1 && esArbolVacio t2 then x : leaves t1 ++ leaves t2 else leaves t1 ++ leaves t2
+
+esArbolVacio :: Tree a -> Bool
+esArbolVacio EmptyT = True
+esArbolVacio _ = False
+
+-----------------------------------------------------------------------------------
+
+heightT :: Tree a -> Int
+heightT EmptyT = 0
+heightT (NodeT x t1 t2) = if esArbolVacio t1 && esArbolVacio t2 then 1 + heightT t1 + heightT t2 else heightT t1 + heightT t2
+
+------------------------------------------------------------------------------------
+mirrorT :: Tree a -> Tree a
+mirrorT EmptyT = EmptyT
+mirrorT (NodeT x t1 t2) = NodeT x (mirrorT t2) (mirrorT t1)
+
+----------------------------------------------------------------------------------
+
+toList :: Tree a -> [a]
+toList EmptyT = []
+toList (NodeT x t1 t2) = toList t1 ++ [x] ++ toList t2
+
+----------------------------------------------------------------------------------
+
+levelN :: Int -> Tree a -> [a]
+levelN _ EmptyT = []
+levelN 0 (NodeT y _ _ ) = [y]
+levelN x (NodeT y t1 t2) = if x > 0 then levelN (x - 1) t1 ++ levelN (x - 1) t2 else y : levelN x t1 ++ levelN x t2
