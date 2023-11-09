66481c5ae1871ebee70d12d771c9cd44eb6a6b3e
Ivan Rodriguez <40413833+IvanRod1@users.noreply.github.com>
2023-11-09 19:53:25 -0300

    Add files via upload

--- Estructuras 2/TP Estructuras 2/UFSet.cpp
+++ Estructuras 2/TP Estructuras 2/UFSet.cpp
@@ -15,8 +15,8 @@
 struct UFNode {
    ELEM_TYPE element;
    // COMPLETAR
-   UFNode* elementPrevio; //Todo ufSET sabe cual es su ufset previo
-   UFNode* elementRepresentante; // Todo ufSET sabe cual es su ufSET representante
+   UFNode* elementPrevio; //Todo ufSET sabe cual es su ufset previo y si no tiene un ufSET previo, el mismo será NULL
+   //UFNode* elementRepresentante; // Todo ufSET sabe cual es su ufSET representante y ese ufSET nunca podrá ser NULL
 };
 
 /* 
@@ -26,8 +26,8 @@ UFSet createUFS(ELEM_TYPE value) {
    // COMPLETAR
    UFNode* ufsetN = new UFNode;
    ufsetN->element = value;
-   ufsetN->elementPrevio = NULL;
-   ufsetN->elementRepresentante = ufsetN;
+   ufsetN->elementPrevio = ufsetN;
+   //ufsetN->elementRepresentante = ufsetN;
 
    return ufsetN;
 }
@@ -44,13 +44,63 @@ ELEM_TYPE elemUFS(UFSet ufset) {
 UFSet findUFS(UFSet elem) {
   
    // COMPLETAR
-    return elem->elementRepresentante;
+   //return elem->elementRepresentante;
+   if(elem->elementPrevio->element == elem->element)
+   {
+      return elem;
+   }
+   else
+   {
+      return findUFS(elem->elementPrevio);
+   }
+
+}
+
+/*UFSet findUFSi(UFSet elem)
+{
+   
+}*/
+
+
+
+
+int rango(UFSet ufset)
+{
+   int contador = 0;
+   UFNode* ufsetAux = ufset;
+   
+   while(ufsetAux->elementPrevio->element != ufsetAux->element)
+   {
+      contador++;
+      ufsetAux = ufsetAux->elementPrevio;
+   }
+
+   delete ufsetAux;
+
+   return contador;
 }
 
 /*
  * Calcula la unión entre los conjuntos ufset1 y ufset2. 
  * Esta operación puede ser optimizada con la técnica de unión por rango.
  */
+
 void unionUFS(UFSet ufset1, UFSet ufset2) {
    // COMPLETAR
+   //findUFS(ufset1) -> elementPrevio = findUFS(ufset2);
+   //findUFS(ufset1) -> elementPrevio = ufset2;
+   //cout << "hola" << endl;
+   if(rango(findUFS(ufset1)) > rango(findUFS(ufset2)))
+   {
+      delete findUFS(ufset2)->elementPrevio;
+      findUFS(ufset2)->elementPrevio = findUFS(ufset1);
+   }
+   else
+   {
+      delete findUFS(ufset1)->elementPrevio;
+      findUFS(ufset1)->elementPrevio = findUFS(ufset2);
+   }
+
 }
+
+
